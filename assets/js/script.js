63ffce00df3694e0fa7376acc235d743af2f7bf0
dubsumm <w.b.summerlin@gmail.com>
2022-06-24 10:11:00 -0400

    attempted to debug error when invalid input is given

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,24 +7,25 @@ var schar = ('!@#$%^&*_-+=');
 
 
 function generatePassword() {
-  var bigPw = lcl;
+  var bigPw = lcl;  //set smallest possible pool of password chars to lower case letters
   var pwLength = window.prompt("How long would you like your password to be? (8 through 128 chars).")
-    if (pwLength >= 8 && pwLength <= 128){
-      var wantUpper = confirm("Hit OK if you would you like your password to include upper case characters.");
-        if (wantUpper === true) {
+    if (pwLength >= 8 && pwLength <= 128){  //chk if usr input is valid
+      var wantUppr = confirm("Hit OK if you would you like your password to include upper case characters.");
+        if (wantUppr === true) { // if usr selects wantUppr add upp case letters to pool
           bigPw = bigPw + ucl;
         }
         var wantNum = confirm("Hit OK if you would you like your password to include numbers.");
-        if (wantNum === true) {
+        if (wantNum === true) { // if usr selects wantNum add nums to pool
           bigPw = bigPw + num;
         }
         var wantSpec = confirm("Hit OK if you would you like your password to include special characters.");
-        if (wantUpper === true) {
+        if (wantUpper === true) { // if usr selects schar add spec chars to pool
           bigPw = bigPw + schar;
         }
-    } else {
+    } else {  // throws alert if intial input is invalid and recallls function
     window.alert("Invalid entry. Please press OK and try again.");
-    generatePassword();
+    writePassword();
+    return;
     }
   console.log("Password Length: " + pwLength);
   console.log("Uppercase: " + wantUpper);
